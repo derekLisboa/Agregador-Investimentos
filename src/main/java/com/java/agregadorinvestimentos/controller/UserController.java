911ec8bd8de6cfd52689a238e8b7cf6697b38b90
Derek Lisboa <derekmlisboa@gmail.com>
2026-02-19 14:51:35 -0300

    Deletar os usuarios pelo ID

--- src/main/java/com/java/agregadorinvestimentos/controller/UserController.java
+++ src/main/java/com/java/agregadorinvestimentos/controller/UserController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,4 +47,11 @@ public class UserController {
         var users = userService.listUsers();
         return ResponseEntity.ok(users);
     }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteById(@PathVariable("userId") String userId){
+        userService.deleteById(userId);
+        return ResponseEntity.noContent().build();
+    }
+
 }
