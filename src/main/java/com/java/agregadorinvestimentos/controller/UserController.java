7c18fe1bbadb582b3fd740343b4fae2aac7f63cb
Derek Lisboa <derekmlisboa@gmail.com>
2026-02-19 14:41:55 -0300

    Listar os Usuarios

--- src/main/java/com/java/agregadorinvestimentos/controller/UserController.java
+++ src/main/java/com/java/agregadorinvestimentos/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.java.agregadorinvestimentos.controller;
 
 import java.net.URI;
+import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,5 +39,11 @@ public class UserController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> listUsers(){
+        var users = userService.listUsers();
+        return ResponseEntity.ok(users);
     }
 }
